f622a9effd0db6832fe84e411bbe9d44c9d8db85
Michelle <michey.asmah21@gmail.com>
2022-05-13 20:30:40 -0400

    styling complete-for larger screens

--- src/pages/Prompts.js
+++ src/pages/Prompts.js
@@ -4,7 +4,6 @@ import Results from "./Results";
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Container from 'react-bootstrap/Container'
 
 const Prompts = () => {
 
@@ -41,50 +40,57 @@ const Prompts = () => {
   }
 
   return (
-    <Row>
-      <Col md={5} id="summary-bg">
-        <header>
-          <h1 className="bg-opacity headers">The Prompt Shop</h1>
-        </header>
-        <summary className="bg-opacity" id="sum">
-          Welcome to The Prompt Shop! Enter a prompt and receive fun responses from the <a href="https://beta.openai.com/" target="_blank" rel="noreferrer">OpenAI</a> API. Click on a preset example prompt below to see a response or try your own!
-        </summary>
-        <form onSubmit={onSubmit} id="prompt-form">
-          <textarea
-            placeholder="Enter a Prompt"
-            form="prompt-form"
-            name="prompt"
-            value={promptInput}
-            onChange={(e) => setPromptInput(e.target.value)}  
-          />
-          <input type="submit" value="Generate Results" />
-          <input type="submit" onClick={(e) => setPromptInput(e.target.value)}value="Say this is a test"></input>
-          <input type="submit" onClick={(e) => setPromptInput(e.target.value)}value="Write a poem about a dog wearing skis"></input>
-        </form>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>           
-        )}
-      </Col>
+    
+      <Row className="row-style">
+        <Col md={5} id="summary-bg" className="res-scroll">
+          <header>
+            <h1 className="bg-opacity headers">The Prompt Shop</h1>
+          </header>
+          <div className="bg-opacity sum-box" >
+            <summary id="sum">
+              Welcome to The Prompt Shop! Enter a prompt and receive fun responses from the <a href="https://beta.openai.com/" target="_blank" rel="noreferrer">OpenAI</a> API. Click on a preset example prompt below to see a response or try your own!
+            </summary>
+            <form onSubmit={onSubmit} id="prompt-form">
+              <input className="gen-btn" type="submit" onClick={(e) => setPromptInput(e.target.value)}value="How does a telescope work?"></input> 
+              <input className="gen-btn" type="submit" onClick={(e) => setPromptInput(e.target.value)}value="Write a poem about a dog wearing skis"></input>
+              <textarea
+                placeholder="Enter a Prompt"
+                form="prompt-form"
+                name="prompt"
+                value={promptInput}
+                onChange={(e) => setPromptInput(e.target.value)}
+                rows="4" 
+                cols="32"
+              />
+              <input className="gen-btn" type="submit" value="Generate Results" />
+    
+            </form>
+            {errorMessage && (
+              <div>
+                <p className="error-text">{errorMessage}</p>
+              </div>           
+            )}
+          </div>
+        </Col>
 
-      <Col md={7} className="res-scroll">
-        <h3 className="headers" id="res-header">Your Results</h3>
-        {results.map(res => {
-            return (
-              <Card key={res.id} className="prompt-card">
-                <Card.Body>      
-                  <Results 
-                    prompt={res.prompt}
-                    result={res.result}   
-                  />
-                </Card.Body>
-              </Card>
-            )
-          })
-        }
-      </Col>
-    </Row>
+        <Col md={7} className="res-scroll">
+          <h3 className="headers" id="res-header">Your Results</h3>
+          {results.map(res => {
+              return (
+                <Card key={res.id} className="prompt-card">
+                  <Card.Body>      
+                    <Results 
+                      prompt={res.prompt}
+                      result={res.result}   
+                    />
+                  </Card.Body>
+                </Card>
+              )
+            })
+          }
+        </Col>
+      </Row>
+  
   );
 }
 
